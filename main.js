76b8385cafd0432020235def1575bcf841df977a
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-03-30 16:29:46 -0400

    Deletes placeholder comments, puts own comments

--- main.js
+++ main.js
@@ -25,55 +25,10 @@ while (count <= gridWidth * gridWidth) {
   count++;
 }
 
-// You probably should NOT do these in the order below.
-// That is, you probably should NOT do all the queries,
-// THEN all the functions,
-// THEN all the wiring.
+// event listener function for what happens when one is clicked.
+//===========DO NOT TOUCH ABOVE CODE===========
 
-// Instead, it'll be easier if you go one action at a time!
-// So, add a query for the palette colors.
-// THEN add an event listener function for what happens when one is clicked.
-// THEN wire those two together, so that when the palette elements are clicked,
-// the function runs.
-//
-// And proceed from there to getting the squares working.
-//
-
-// ALSO.
-// You do not have to follow the sections below. If you're doing your functions inline, it doesn't make a lot of sense to separate the event listener functions from their wiring!
-
-/***********
- * QUERIES *
-***********/
-
-// Add queries for all your squares, palette colors, and brush here.
-// (Note the singular or plural used in that sentence!)
-
-
-
-
-/****************************
- * EVENT LISTENER FUNCTIONS *
-****************************/
-
-// Now add some functions to handle clicking one particular square
-// and clicking one particular palette color. You can leave them
-// empty at first, though a console.log just to know they're being
-// run as event listeners (after the next step is set up) isn't a
-// bad idea for testing purposes.
-
-
-
-/**************************
- * WIRING IT ALL TOGETHER *
-**************************/
-
-// Now: wiring up our event listeners to our html node elements.
-// You'll need to add the appropriate event listener for each
-// square and for each palette color from the functions you
-// wrote above.
-
-//queries the canvas squares
+//query the canvas squares
 const canvasSquares = document.querySelectorAll('.canvas');
 //loop through all the canvas squares and create eventlistener click event
 for (square of canvasSquares) {
@@ -93,7 +48,19 @@ for (palette of paletteColors) {
 
 //query the main brush
 const currentBrush = document.querySelector(".current-brush");
-
+//console log on brush
 currentBrush.addEventListener('click', function(){
   console.log('You clicked the current brush')
 })
+
+//Functions that will replace console logs below
+//============================================
+// create a function that will replace the current brush with the palette clicked
+//this function will grab the "second class of the palette which holds the color in css e.g. color-1 is lightblue"
+function grabColor(e) {
+  return e.classList[1];
+}
+//
+function whenSquareClicked(e) {
+  const square = e.target
+}
